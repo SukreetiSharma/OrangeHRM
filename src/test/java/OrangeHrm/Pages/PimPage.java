4c446da4d55cf0187d7463332219d0ef143a866c
SukreetiSharma <sukreeti.sharma@infostride.com>
2022-09-07 19:57:04 +0530

    orangeHrm 2

--- src/test/java/OrangeHrm/Pages/PimPage.java
+++ src/test/java/OrangeHrm/Pages/PimPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 public class PimPage {
     WebDriver driver;
-    By admin = By.xpath("(//span[@class=\"oxd-text oxd-text--span oxd-main-menu-item--name\"])[1]");
+    By admin = By.xpath("(//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name'])[1]");
     By Add = By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary']");
     public PimPage(WebDriver driver){
 
